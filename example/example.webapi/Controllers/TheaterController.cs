6747c7738b53f89e0e25f902833e09b4c4b45ba0
ana_senjug <ana.senj13@gmail.com>
2023-07-10 09:05:51 +0200

    Edited the webapi in my example folder

--- example/example.webapi/Controllers/TheaterController.cs
+++ example/example.webapi/Controllers/TheaterController.cs
@@ -21,7 +21,13 @@ namespace example.webapi.Controllers
         }
         public HttpResponseMessage GetTheaters()
         {
-            
+            try
+            {
+                List<Theater> theaters = iTheaterServices.ListTheaters();
+                if (theaters == null) throw new Exception();
+                return Request.CreateResponse(HttpStatusCode.OK, theaters);
+            }
+            catch { return Request.CreateResponse(HttpStatusCode.InternalServerError,"Code not working"); }   
             
 
         }
@@ -29,26 +35,42 @@ namespace example.webapi.Controllers
         // GET: api/Theater/5
         public HttpResponseMessage GetTheaterById(Guid id)
         {
-           
+            if (id == null) return Request.CreateResponse(HttpStatusCode.BadRequest);
+            Theater theater =iTheaterServices.GetTheaterById(id);
+
+            if (theater == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, theater);
+            }
         }
 
         [HttpPost]
         public HttpResponseMessage AddTheater([FromBody] Theater theater)
         {
-            
+            if (theater == null) return Request.CreateResponse(HttpStatusCode.BadRequest);
+            theater=iTheaterServices.AddTheater(theater);
+            if(theater==null) return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            return Request.CreateResponse(HttpStatusCode.OK,theater);
         }
 
         [HttpPut]
         public HttpResponseMessage ChangeName(Guid id, [FromBody] Theater updatedTheater)
         {
-           
+            if (updatedTheater == null) return Request.CreateResponse(HttpStatusCode.BadRequest);
+            if (iTheaterServices.ChangeName(id,updatedTheater) == true) return Request.CreateResponse(HttpStatusCode.OK, updatedTheater);
+
+            return Request.CreateResponse(HttpStatusCode.InternalServerError);
         }
 
         // DELETE: api/Theater/5
         public HttpResponseMessage Delete(Guid id)
         {
-
-
+            if (iTheaterServices.Delete(id) == true) return Request.CreateResponse(HttpStatusCode.OK, "Theater has been deleted successfully");
+            return Request.CreateResponse(HttpStatusCode.InternalServerError);  
             
         }
     }
